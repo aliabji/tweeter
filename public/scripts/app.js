4a90584c70408d49500b3bc7e1817522d926672f
Ali Abji <aliabji@gmail.com>
2017-09-20 20:04:33 +0000

    tweets posting to db and refetching automatically, form validation complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,40 +5,6 @@
  */
 $(function () {
 
-  function escape(str) {
-    let div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  }
-
-  //function to input the tweet data into the tweet template
-  const createTweetElement = function(tweet) {
-    let $tweet =    
-  
-      ` <article class=“posted-tweets”>
-          <header>
-            <div>
-              <img src=${escape(tweet.user.avatars.small)} >
-            </div>
-            <h2>${escape(tweet.user.name)}</h2>
-            <h4> ${escape(tweet.user.handle)}</h4>
-          </header>
-          <div>
-           <p>${escape(tweet.content.text)}</p>
-          </div>
-
-          <footer>
-            <p>${escape(tweet.created_at)}
-            <i class="fa fa-heart" aria-hidden="true"></i>
-            <i class="fa fa-retweet" aria-hidden="true"></i>
-            <i class="fa fa-flag" aria-hidden="true"></i>
-            </p>
-          </footer>
-        </article>`
-    
-   return $tweet
-  }
-
   let data = [
     {
       "user": {
@@ -85,6 +51,40 @@ $(function () {
       "created_at": 1461113796368
     }
   ];
+  //form data security
+  const escape = function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
+  //function to input the tweet data into the tweet template
+  const createTweetElement = function(tweet) {
+    let $tweet =    
+  
+      ` <article class=“posted-tweets”>
+          <header>
+            <div>
+              <img src=${escape(tweet.user.avatars.small)} >
+            </div>
+            <h2>${escape(tweet.user.name)}</h2>
+            <h4> ${escape(tweet.user.handle)}</h4>
+          </header>
+          <div>
+           <p>${escape(tweet.content.text)}</p>
+          </div>
+
+          <footer>
+            <p>${escape(tweet.created_at)}
+            <i class="fa fa-heart" aria-hidden="true"></i>
+            <i class="fa fa-retweet" aria-hidden="true"></i>
+            <i class="fa fa-flag" aria-hidden="true"></i>
+            </p>
+          </footer>
+        </article>`
+    
+   return $tweet
+  }
 
   //taking in the array of tweets and utilizing the createTweetElement function
   //to render the tweets within the template
@@ -94,18 +94,8 @@ $(function () {
       $('.tweet-container').append($tweet);
     }
   }
-
-
-
-  //prevent page from changing after submitting new tweet and storing data in new var
-  let $form = $('#submit');
-  $form.on('submit', function (ev) {
-    ev.preventDefault()
-    let formData = $form.serialize()
-    return formData
-  })
-
-  function loadTweets() {
+  
+  const loadTweets = function() {
     $.get('/tweets')
     .done(function(result) {
       $('.tweet-container').empty()
@@ -115,6 +105,35 @@ $(function () {
       console.error(error)
     })
   }
+
+  //
+  const postTweets = function(formData) {
+    $.post('/tweets', formData)
+    .done(function(result) {
+      loadTweets()
+    })
+    .fail(function(error) {
+      console.log(error)
+    })
+  }
+
+  //prevent page from changing after submitting new tweet and storing data in tweet database,
+  //form validation
+  let $form = $('#submit');
+  $form.on('submit', function (ev) {
+    ev.preventDefault()
+    console.log($('.tweet-text').val())
+    if ($('.tweet-text').val().length == 0) {
+      alert("Please enter your tweet")
+      return
+    } else if ($('.tweet-text').val().length > 140) {
+      alert("Please enter 140 characters or less")
+      return
+    } else {
+      let formData = $form.serialize()
+      postTweets(formData)
+    }
+  })
 
   loadTweets()
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ let totalChars = 0
 //checking text box for chars, updating counter in bottom right corner of compose
 //if counter is less than 0, number turns red. when counter is zero or greater, red colour is //removed
 $(document).ready(function(textArea) {
-  $('.text-area').keyup(function () {
+  $('.tweet-text').keyup(function () {
     totalChars = this.value.length
     $('.counter').html(140 - totalChars)
     let number = parseInt($('.counter').text());
